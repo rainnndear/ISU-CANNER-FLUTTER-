a087c9b2bb48b1a62016968a31454e0aefcd0389
rainier <rainieraquino1451@gmail.com>
2024-09-09 08:42:43 +0800

    Refactor client homepage to display QR code with user details

--- lib/screens/client/client_Homepage.dart
+++ lib/screens/client/client_Homepage.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:isu_canner/services/logout.dart';
 import 'package:isu_canner/model/user.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class ClientHomepage extends StatefulWidget {
-  final User user; 
+  final User user;
 
   const ClientHomepage({Key? key, required this.user}) : super(key: key);
 
@@ -14,14 +15,18 @@ class ClientHomepage extends StatefulWidget {
 class _ClientHomepageState extends State<ClientHomepage> {
   @override
   Widget build(BuildContext context) {
+    // Combine user details into a string for the QR code
+    String userDetails = 'Name: ${widget.user.firstname} ${widget.user.lastname}\n'
+        'Email: ${widget.user.email}';
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Welcome, ${widget.user.email}!'), 
+        title: Text('Welcome, ${widget.user.email}!'),
         actions: [
           IconButton(
             icon: const Icon(Icons.logout),
             onPressed: () async {
-              await logout(context); 
+              await logout(context);
             },
           ),
         ],
@@ -30,8 +35,16 @@ class _ClientHomepageState extends State<ClientHomepage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Hello, ${widget.user.firstname} ${widget.user.lastname}'), 
+            Text('Hello, ${widget.user.firstname} ${widget.user.lastname}'),
             const SizedBox(height: 20),
+            // Display the QR code with user details
+            QrImageView(
+              data: userDetails, // Data to encode in the QR code
+              version: QrVersions.auto,
+              size: 200.0, // Size of the QR code
+            ),
+            const SizedBox(height: 20),
+            Text('Scan this QR code for user details'),
           ],
         ),
       ),
